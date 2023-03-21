f65e9463ddcf873a103ea46eaa924ff05c8cb3a7
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-03-21 19:08:43 +0530

    Add files via upload

---
+++ breaking.py
@@ -0,0 +1,27 @@
+for val in "string":
+    if(val=='i'):
+        break
+    print(val)
+print("the end")
+
+
+
+for f in "string":
+    if(f=='i'):
+        break
+    print(f)
+print("the end")
+
+for val in "string":
+    if(val=='i'):
+        continue
+    print(val)
+print("the end")
+
+
+i=1
+while(i<6):
+    print(i)
+    if(i==3):
+        break
+    i+=1
